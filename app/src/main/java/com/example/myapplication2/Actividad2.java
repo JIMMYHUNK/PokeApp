9834390827d4d21f8cedf6e0a3bedcd68ce5c1f3
manuelrojasm <manuelrojasm13@gmail.com>
2022-11-08 23:50:58 -0500

    poké me trasnoche

--- app/src/main/java/com/example/myapplication2/Actividad2.java
+++ app/src/main/java/com/example/myapplication2/Actividad2.java
@@ -1,76 +1,96 @@
 package com.example.myapplication2;
 
-import static com.android.volley.Request.Method.GET;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Random;
 
 public class Actividad2 extends AppCompatActivity {
-    private TextView tv1;
+    private TextView name;
+    private TextView link;
+    private TextView tipe;
     private RequestQueue rq;
+    private RequestManager glide;
+    private ImageView image;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad2);
-        tv1=findViewById(R.id.tv1);
+        name = findViewById(R.id.vname);
+        link = findViewById(R.id.vurl);
+        tipe = findViewById(R.id.vtipe);
+        image = findViewById(R.id.imageView4);
         rq = Volley.newRequestQueue(this);
+        glide = Glide.with(this);
+        this.traerPokemones();
     }
 
-    public void recuperar (View v)
-    {
-        tv1.setText("");
-        String url="https://pokeapi.co/api/v2/pokemon/${5}";
-        JsonArrayRequest requerimiento=new JsonArrayRequest(Request.Method.GET,
+    public void traerPokemones() {
+        name.setText("");
+        link.setText("");
+        tipe.setText("");
+        Random random = new Random();
+        int pokemonRandom = random.nextInt(905) + 1;
+        String url = "https://pokeapi.co/api/v2/pokemon/" + pokemonRandom;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
                 url,
                 null,
-                new Response.Listener<JSONArray>() {
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-
-                        for(int f=0;f<response.length();f++)
-                        {
-                            try {
-                                JSONObject objeto = new JSONObject(response.get(f).toString());
-                                tv1.append("name:"+objeto.getString("name")+"\n");
-                                tv1.append("url:"+objeto.getString("url")+"\n");
-                                tv1.append("______________________________\n");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                    public void onResponse(JSONObject response) {
+                        try {
+                            name.append(response.getString("name"));
+                            link.append(url.toString());
+                            glide.load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+pokemonRandom+".png").into(image);
+                            for(int f=0;f<response.getJSONArray("types").length();f++)
+                            {
+                                try {
+                                    JSONObject objeto = new JSONObject(response.getJSONArray("types").get(f).toString());
+                                    tipe.append(objeto.getJSONObject("type").getString("name")+"\n");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        }
 
+
+                            tipe.append(response.getJSONObject("types").getJSONObject("type").getString("name"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                },
-                new Response.ErrorListener() {
+                }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(Actividad2.this,error.getMessage(),Toast.LENGTH_LONG).show();
-
+                        error.printStackTrace();
                     }
                 });
-        rq.add(requerimiento);
+        rq.add(jsonObjectRequest);
+
+
+
+    }
+
+    public void recuperar(View v) {
+        this.traerPokemones();
     }
 }
